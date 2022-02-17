698268c546be335c92d0f72d9f9de89660d70ad2
Krzysztof Branecki <krzysztof.branecki@frontkom.com>
2022-02-17 09:55:36 +0100

    DEVOPS-16 revert command names

--- command.php
+++ command.php
@@ -21,6 +21,6 @@ WP_CLI::add_command('build', Build_Command::class, array(
 	'shortdesc' => 'Parse the build file and download, install or update the core, plugins or themes on it.'
 ));
 
-WP_CLI::add_command('build', Build_Generate_Command::class, array(
+WP_CLI::add_command('build-generate', Build_Generate_Command::class, array(
 	'shortdesc' => 'Generates a build file according to current WordPress installation.'
 ));
